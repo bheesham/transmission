583e4d1afa314413fc75c189041ba952d5251713
Bheesham Persaud <me@bheesham.com>
2023-02-15 23:22:00 -0500

    Add default.nix
    Includes updated versions of libraries required to build Transmission
    from scratch.

---
+++ default.nix
@@ -0,0 +1,33 @@
+{ p ? import <nixpkgs> {} }:
+let
+  pkgs = import (p.fetchFromGitHub {
+    owner = "bheesham";
+    repo = "nixpkgs";
+    rev = "894fa04458a48412a575154bc6986f8fdd62db0e";
+    sha256 = "MjE0JaFVYamxe45Z1sDHMShk21jjxSCTa5Sdk4w8Mwg=";
+  }) p;
+in
+  with pkgs;
+  mkShell {
+    nativeBuildInputs = [ cmake pkg-config python3 git ];
+    buildInputs = [
+      curl
+      dht
+      inotify-tools
+      libb64
+      libdeflate
+      libevent
+      libiconv
+      libnatpmp
+      libpsl
+      libutp
+      miniupnpc
+      openssl
+      pcre
+      systemd
+      zlib
+      # Helpers
+      clang-tools
+      nodejs
+    ];
+  }
